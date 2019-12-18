bc6553750628b23072c422f6e3c8b7fa8775daaf
Agent-003 <abbasova003@gmail.com>
2019-12-18 15:41:20 +0200

    update Mailer.php

--- solid/Mailer.php
+++ solid/Mailer.php
@@ -1,19 +1,88 @@
 <?php
 
-//Hint - Dependency Inversion Principle
-class Mailer
+// Hint - Dependency Inversion Principle
+// Принцип инверсии зависимостей
+
+interface Mailer
 {
+    public function send();
+    public function getWelcomeMessage();
 }
 
 class SendWelcomeMessage
 {
     private $mailer;
+
     public function __construct(Mailer $mailer)
     {
         $this->mailer = $mailer;
     }
+
+    public function sendWelcome()
+    {
+       return $this->mailer->getWelcomeMessage();
+    }
+
 }
 
-//SendWelcomeGoolge
-//SendWelcomeSendgrid
-//SendWelcomeMailchimp
+class Goolge implements Mailer
+{
+
+    public $welcomeMessage="Welcome from Goolge";
+    public $message;
+    public $settings = ['host' => 'smtp.google.com',
+                        'user' => 'Google',
+                        'password' => 'googlepass'];
+
+    public function send()
+    {
+        return $this->message;
+    }
+
+    public function getWelcomeMessage() {
+        return "Welcome from ".$this->settings['user'];
+    }
+}
+
+class Sendgrid implements Mailer
+{
+
+    public $welcomeMessage="Welcome from Sendgrid";
+    public $message;
+    public $settings = ['host' => 'smtp.sendgrid.com',
+                        'user' => 'Sendgrid',
+                        'password' => 'sendgridpass'];
+
+    public function send()
+    {
+        return $this->message;
+    }
+
+    public function getWelcomeMessage() {
+        return "Welcome from ".$this->settings['user'];
+    }
+}
+
+class Mailchimp implements Mailer
+{
+    public $welcome="Welcome send from Mailchimp";
+    public $message;
+    public $settings = ['host' => 'smtp.mailchimp.com',
+                        'user' => 'Mailchimp',
+                        'password' => 'mailchimppass'];
+
+    public function send()
+    {
+        return $this->message;
+    }
+
+    public function getWelcomeMessage() {
+        return "Welcome from ".$this->settings['user'];
+    }
+}
+
+$SendWelcomeGoolge=new Goolge;
+$SendWelcomeSendgrid=new Sendgrid;
+$SendWelcomeMailchimp=new Mailchimp;
+$send=New SendWelcomeMessage($SendWelcomeMailchimp);
+echo $send->sendWelcome();
